fa97f784ee61692fa7b6fb21017963d2ba6a00a3
freewind <nowindlee@gmail.com>
2018-07-29 22:07:43 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Java Hello World Demo
-=====================
+Java CommonMark Demo
+====================
+
+Use [commonmark-java](https://github.com/atlassian/commonmark-java) to render markdown text to html.
 
 Run `Hello.java` in your IDE.
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,25 @@
 package demo;
 
+import org.apache.commons.io.IOUtils;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        String md = readMarkdownFile();
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(md);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        String html = renderer.render(document);
+        System.out.println(html);
+    }
+    private static String readMarkdownFile() throws IOException {
+        return IOUtils.toString(Hello.class.getClassLoader().getResourceAsStream("hello.md"), Charset.defaultCharset());
     }
 
 }
